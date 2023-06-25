226db934e97ec52770b06d01497483b8bad7debf
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-06-25 07:01:25 +0530

    style props

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Flex, Box} from "@chakra-ui/react"
+
+
+export const Navbar = () => {
+  return (
+    <div>
+
+    <Flex>
+        
+    </Flex>
+        {/* <Flex bg="gray" justify="space-between" gap="2px"  wrap="wrap">
+            <Box w="150px" h="90px" bg="red">1</Box>
+            <Box w="150px" h="90px" bg="blue">2</Box>
+            <Box w="150px" h="90px" flexGrow="1" bg="green">3</Box>
+            <Box w="150px" h="90px" flexGrow="2" bg="pink">4</Box>
+        </Flex> */}
+    </div>
+  )
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,35 @@
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+
 export default function Dashboard() {
+  const getStyle = {
+    p: "50px",
+    m: "10px",
+    bg: "pink.400",
+    fontSize: "2xl",
+    filter: "blur(2px)",
+    ":hover": {
+      bg:"pink.50",
+      fontSize:"6xl"
+    }
+  };
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <Container as="section" my="20px">
+        <Heading p="20px" my="10px">
+          Chakra UI Heading
+        </Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+          repellat? Officiis voluptatem qui est placeat sit ea, ipsum commodi
+          fugiat accusamus a nesciunt aliquid culpa unde aspernatur dolorum,
+          itaque tempore.
+        </Text>
+
+        <Box color="blue.500" p="20px" m="10px" bg="orange.50" fontSize="40px">
+          <Text>This is a Box</Text>
+        </Box>
+        <Box sx={getStyle}>This is new box</Box>
+      </Container>
+    </div>
+  );
 }
