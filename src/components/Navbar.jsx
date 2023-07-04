c43ea98c6fa526f8d20233a537972659a9bc792c
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-07-04 23:43:26 +0530

    initial update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,23 @@
-import { Flex, Box} from "@chakra-ui/react"
-
+import { Flex, Box, Text, Button, Heading, Spacer,HStack} from "@chakra-ui/react";
 
 export const Navbar = () => {
   return (
     <div>
-
-    <Flex>
-        
-    </Flex>
-        {/* <Flex bg="gray" justify="space-between" gap="2px"  wrap="wrap">
+      <Flex as="nav" p="10px" alignItems="center">
+        <Heading as="h1">MyTasks</Heading>
+        <Spacer />
+        <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">B</Box>
+        <Text>bhumi@gmail.com</Text>
+        <Button colorScheme="messenger">Logout</Button>
+        </HStack>
+      </Flex>
+      {/* <Flex bg="gray" justify="space-between" gap="2px"  wrap="wrap">
             <Box w="150px" h="90px" bg="red">1</Box>
             <Box w="150px" h="90px" bg="blue">2</Box>
             <Box w="150px" h="90px" flexGrow="1" bg="green">3</Box>
             <Box w="150px" h="90px" flexGrow="2" bg="pink">4</Box>
         </Flex> */}
     </div>
-  )
-}
+  );
+};
